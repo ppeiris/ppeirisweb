1730dff9b7e9ff0bd8601c57645819af1c298e12
Prabath Peiris <prabath.peiris@ftr.com>
2016-11-27 01:06:31 -0500

    init script

--- initsetup.sh
+++ initsetup.sh
@@ -1,56 +1,48 @@
 #!/usr/bin/env bash
 
-branch_name='master1'
+message() {
+	printf "\033[1;31m => %s\n\033[0m" "$1"
+}
 
-istmpBranch=`git branch --list $branch_name`
+tmpbranch="tmpbranch"
+SOURCE=ppeiris.src
+REPO=https://github.com/ppeiris/ppeirisweb.git
+USERNAME=ppeiris
 
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-
-echo $branch
-
-
-# if [  ]; then
-#    echo "Branch name $branch_name already exists."
-# fi
-
-# message() {
-# 	printf "\033[1;32m :: %s\n\033[0m" "$1"
-# }
-
-# GITUSER=ppeiris
-# REPONAME=https://github.com/ppeiris/ppeirisweb.git
-# HUGOSRC=ppeiris.src
-
-# # delete the master branch
-# message "Deleting the \`master\` branch"
-# git branch -D master
-# git push origin --delete master
+if git show-ref --verify --quiet "refs/heads/$tmpbranch"; then
+	message "tmp Branch $tmpbranch already exists."
+	message "Deleting existing $tmpbranch ..."
+	git checkout master
+	git branch -D $tmpbranch
+	message "Creating a $tmpbranch"
+	git branch $tmpbranch
+	git checkout $tmpbranch
+fi
 
 
-# # create a orphaned master branch
-# message "Creating orphaned master branch"
-# git checkout --orphan master
-# git rm --cached $(git ls-files)
+message "Deleting the \`master\` branch"
+git branch -D master
+git push origin --delete master
 
-# # checkout the src from the README.md file from the src branch
-# message "Grabing README file from the \`$HUGOSRC\` branch"
-# git checkout "$HUGOSRC" README.md
-# git commit -m "Initial commit on master branch with the README file"
+message "Creating an empty, orphaned \`master\` branch"
+git checkout --orphan master
+git rm --cached $(git ls-files)
 
-# # push to master branch
-# git push origin master
+message "Grabbing one file from the \`$SOURCE\` branch so that a commit can be made"
+git checkout "$SOURCE" README.md
+git commit -m "Initial commit on master branch"
+git push origin master
 
-# # checkout the src branch
-# git checkout -f "$HUGOSRC"
+message "Returning to the \`$SOURCE\` branch"
+git checkout -f "$SOURCE"
 
-# # remove the public dir
-# rm -rf public
-# git add -u
-# git commit -n "Remove public folder"
+message "Removing the \`public\` folder to make room for the \`master\` subtree"
+rm -rf public
+git add -u
+git commit -m "Remove stale public folder"
 
-# # add the master branch as a subtree
-# message "Add the master branch as a subtree"
-# git subtree add --prefix=public --squash $REPONAME master
+message "Adding the new \`master\` branch as a subtree"
+git subtree add --prefix=public $REPO master --squash
 
-# message "Pulling down the just committed file to help avoid merge conflicts"
-# git subtree pull --prefix=public $REPONAME master
+message "Pulling down the just committed file to help avoid merge conflicts"
+git subtree pull --prefix=public $REPO master
