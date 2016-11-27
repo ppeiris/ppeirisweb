3b0f668983c64db0daf50f4f754f1d2565840300
Prabath Peiris <prabath.peiris@ftr.com>
2016-11-26 23:42:36 -0500

    working on the init script

---
+++ initsetup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+GITUSER=ppeiris
+HUGOSRC=ppeiris.src
+
+# delete the master branch
+
+
+# create a orphaned master branch
+
+
+# checkout the src from the README.md file from the src branch
+
+
+# push to master branch
+
+# checkout the src branch
+
+# remove the public dir
+
+# add the master branch as a subtree
+
